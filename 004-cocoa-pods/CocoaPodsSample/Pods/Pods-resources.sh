cd94ed7dfdc39b6de1f1bd1fdae3fcb1ec5c5e18
Ben Scheirman <ben@scheirman.com>
2012-02-12 19:22:24 -0600

    Finished sample

---
+++ 004-cocoa-pods/CocoaPodsSample/Pods/Pods-resources.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+install_resource()
+{
+  case $1 in
+    *.xib)
+      echo "ibtool --errors --warnings --notices --output-format human-readable-text --compile ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xib`.nib ${SRCROOT}/Pods/$1 --sdk ${SDKROOT}"
+      ibtool --errors --warnings --notices --output-format human-readable-text --compile ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xib`.nib ${SRCROOT}/Pods/$1 --sdk ${SDKROOT}
+      ;;
+    *)
+      echo "cp -R ${SRCROOT}/Pods/$1 ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+      cp -R "${SRCROOT}/Pods/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+      ;;
+  esac
+}
+install_resource 'SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle'
