4d67e948a59f434c58dc57c99bc3c23c3158ab61
Ben Scheirman <ben@scheirman.com>
2012-02-16 20:28:19 -0600

    preparation for pushing episode 3

--- 004-cocoa-pods/CocoaPodsSample/Pods/Headers/libPusher/NSString+Hashing.h
+++
@@ -1,1 +0,0 @@
-../../libPusher/Library/NSString+Hashing.h
--- 004-cocoa-pods/CocoaPodsSample/Pods/libPusher/Library/NSString+Hashing.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSString (Hashing)
-- (NSString *)MD5Hash;
-- (NSString *)HMACDigestUsingSecretKey:(id)secretKey;
-@end
