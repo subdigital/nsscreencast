efab982d11607da946317e38394c3448c9ae6894
Ben Scheirman <ben@scheirman.com>
2012-04-21 15:07:34 -0500

    episode 13 sample code

---
+++ 013-jenkins/MrJenkins/MrJenkins/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MrJenkins
+//
+//  Created by Ben Scheirman on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 013-jenkins/MrJenkins/MrJenkinsTests/MrJenkinsTests.h
@@ -0,0 +1,13 @@
+//
+//  MrJenkinsTests.h
+//  MrJenkinsTests
+//
+//  Created by Ben Scheirman on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MrJenkinsTests : SenTestCase
+
+@end
