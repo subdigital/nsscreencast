fa38ebfda6de397660a315dd2a74e6c4e189ea7a
Ben Scheirman <ben@scheirman.com>
2012-05-24 08:51:29 -0500

    Episode 17 sample code

---
+++ 017-app-provisioning/nsscreencast/nsscreencast/FKBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FKBAppDelegate.h
+//  nsscreencast
+//
+//  Created by Ben Scheirman on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FKBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 017-app-provisioning/nsscreencast/nsscreencast/RootViewController.m
@@ -0,0 +1,42 @@
+//
+//  RootViewController.m
+//  nsscreencast
+//
+//  Created by Ben Scheirman on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"NSScreencast";    
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %d", indexPath.row];
+    return cell;
+}
+
+@end
