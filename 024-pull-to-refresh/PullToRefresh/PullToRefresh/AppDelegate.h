dd88531fd2938b18b52e4c2caea422a60c14ae6b
Ben Scheirman <ben@scheirman.com>
2012-07-08 13:58:56 -0500

    Bare project

---
+++ 024-pull-to-refresh/PullToRefresh/PullToRefresh/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PullToRefresh
+//
+//  Created by Ben Scheirman on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 024-pull-to-refresh/PullToRefresh/PullToRefresh/MasterViewController.m
@@ -0,0 +1,57 @@
+//
+//  MasterViewController.m
+//  PullToRefresh
+//
+//  Created by Ben Scheirman on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+#import "DetailViewController.h"
+
+@interface MasterViewController () {
+    NSMutableArray *_objects;
+}
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+}
+
+- (void)insertNewObject:(id)sender {
+    if (!_objects) {
+        _objects = [[NSMutableArray alloc] init];
+    }
+    [_objects insertObject:[NSDate date] atIndex:0];
+}
+
+#pragma mark - Table View
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _objects.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+
+    NSDate *object = [_objects objectAtIndex:indexPath.row];
+    cell.textLabel.text = [object description];
+    return cell;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        NSDate *object = [_objects objectAtIndex:indexPath.row];
+        [[segue destinationViewController] setDetailItem:object];
+    }
+}
+
+@end
