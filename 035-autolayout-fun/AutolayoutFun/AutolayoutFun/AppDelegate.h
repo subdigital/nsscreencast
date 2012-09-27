1b3b22113e963e5e7c3c29ec5107bb990141a066
Ben Scheirman <ben@scheirman.com>
2012-09-26 23:18:21 -0500

    Added epsisode 35 sample code

---
+++ 035-autolayout-fun/AutolayoutFun/AutolayoutFun/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AutolayoutFun
+//
+//  Created by Ben Scheirman on 9/25/12.
+//  Copyright (c) 2012 NSScreencast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ 035-autolayout-fun/AutolayoutFun/AutolayoutFun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutolayoutFun
+//
+//  Created by Ben Scheirman on 9/25/12.
+//  Copyright (c) 2012 NSScreencast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
