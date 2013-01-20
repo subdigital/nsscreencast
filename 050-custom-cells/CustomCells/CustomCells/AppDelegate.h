edfb78eba703098e81056db931acf6ad2a703685
Ben Scheirman <ben@scheirman.com>
2013-01-20 15:12:56 -0600

    ep 50 starting point

---
+++ 050-custom-cells/CustomCells/CustomCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCells
+//
+//  Created by ben on 1/20/13.
+//  Copyright (c) 2013 NSScreencast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
