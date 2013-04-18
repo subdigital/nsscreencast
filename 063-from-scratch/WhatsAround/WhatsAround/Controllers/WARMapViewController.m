105d5ef41b42eecf69751dc7bd6084b3425f193b
Ben Scheirman <ben@scheirman.com>
2013-04-17 21:26:58 -0500

    Episode 63

---
+++ 063-from-scratch/WhatsAround/WhatsAround/Controllers/WARMapViewController.m
@@ -0,0 +1,25 @@
+//
+//  WARMapViewController.m
+//  WhatsAround
+//
+//  Created by ben on 4/16/13.
+//  Copyright (c) 2013 NSScreencast. All rights reserved.
+//
+
+#import "WARMapViewController.h"
+
+@interface WARMapViewController ()
+
+@end
+
+@implementation WARMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
---
+++ 063-from-scratch/WhatsAround/WhatsAround/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhatsAround
+//
+//  Created by ben on 4/16/13.
+//  Copyright (c) 2013 NSScreencast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WARAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WARAppDelegate class]));
+    }
+}
