6ae630b60942f6df547a687e2b115ee08b454844
Ben Scheirman <ben@scheirman.com>
2013-09-25 22:03:18 -0500

    Episode 087

---
+++ 087-xcode5-autolayout-improvements/AutoLayoutImprovements-starting/AutoLayoutImprovements/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayoutImprovements
+//
+//  Created by ben on 9/22/13.
+//  Copyright (c) 2013 Fickle Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
