64a9df509bf529adf9edab9bbb1cdf2de76a3bfc
Ben Scheirman <ben@scheirman.com>
2013-10-30 22:16:38 -0500

    Episode 93

---
+++ 093-background-transfers/ImageDownloader/ImageDownloader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageDownloader
+//
+//  Created by ben on 10/28/13.
+//  Copyright (c) 2013 Fickle Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
