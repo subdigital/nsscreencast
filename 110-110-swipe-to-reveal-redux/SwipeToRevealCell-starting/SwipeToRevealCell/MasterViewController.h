13478a035a2bd913e6389a193f749c576348b4d0
Ben Scheirman <ben@scheirman.com>
2014-03-03 22:16:02 -0600

    110 starting point

---
+++ 110-110-swipe-to-reveal-redux/SwipeToRevealCell-starting/SwipeToRevealCell/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  SwipeToRevealCell
+//
+//  Created by ben on 2/10/14.
+//  Copyright (c) 2014 NSScreencast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
