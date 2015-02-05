54d47b056be51699b9df36b2ae229e8f17b7a8df
Ben Scheirman <ben@scheirman.com>
2015-02-05 09:03:19 -0600

    Episode 155

---
+++ 155-xcconfig-files-part-2/ConfigDemo/ConfigDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConfigDemo
+//
+//  Created by Ben Scheirman on 1/24/15.
+//  Copyright (c) 2015 NSScreencast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
