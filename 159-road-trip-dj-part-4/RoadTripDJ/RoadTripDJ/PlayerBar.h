887cf153e005fc8528fecdbd283d89d73e6af9e0
Ben Scheirman <ben@scheirman.com>
2015-03-09 09:45:18 -0500

    Episode 159

---
+++ 159-road-trip-dj-part-4/RoadTripDJ/RoadTripDJ/PlayerBar.h
@@ -0,0 +1,16 @@
+//
+//  PlayerBar.h
+//  RoadTripDJ
+//
+//  Created by Ben Scheirman on 2/15/15.
+//  Copyright (c) 2015 Fickle Bits, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface PlayerBar : UIToolbar
+
+@property (nonatomic, assign) IBInspectable CGFloat spacing;
+
+@end
