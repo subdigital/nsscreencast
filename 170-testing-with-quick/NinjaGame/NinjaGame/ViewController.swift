3a3df3babf9bb3dd5f84ca710205210ff9ede8fd
Ben Scheirman <ben@scheirman.com>
2015-05-21 10:25:09 -0500

    170

---
+++ 170-testing-with-quick/NinjaGame/NinjaGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NinjaGame
+//
+//  Created by Ben Scheirman on 5/19/15.
+//  Copyright (c) 2015 NSScreencast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 170-testing-with-quick/NinjaGame/Pods/Target Support Files/Pods-NinjaGameTests/Pods-NinjaGameTests-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Nimble
+#define COCOAPODS_POD_AVAILABLE_Nimble
+#define COCOAPODS_VERSION_MAJOR_Nimble 0
+#define COCOAPODS_VERSION_MINOR_Nimble 4
+#define COCOAPODS_VERSION_PATCH_Nimble 2
+
+// Quick
+#define COCOAPODS_POD_AVAILABLE_Quick
+#define COCOAPODS_VERSION_MAJOR_Quick 0
+#define COCOAPODS_VERSION_MINOR_Quick 3
+#define COCOAPODS_VERSION_PATCH_Quick 1
+
