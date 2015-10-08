dd684b1db502ec06cda368aa446ce310c6f6b026
Ben Scheirman <ben@scheirman.com>
2015-10-08 10:07:45 -0500

    Episode 191

---
+++ 191-swift-2-optionals/Swift2Optionals.playground/Contents.swift
@@ -0,0 +1,23 @@
+var x: Int?
+// x = 12
+
+if let z = x where z % 2 == 0 {
+   var y = z * 2
+}
+
+
+let n = 5
+
+let m = n.advancedBy(5)
+
+if let p = x?.advancedBy(5) {
+    
+}
+
+if let zip = customer?.addresses?.first?.zipCode {
+    
+}
+
+
+
+
