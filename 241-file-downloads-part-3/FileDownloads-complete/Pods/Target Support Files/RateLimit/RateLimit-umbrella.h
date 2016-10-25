6b50afbd0f2d6891b23263326acca9b45d6b2fcc
Ben Scheirman <ben@scheirman.com>
2016-10-25 10:14:16 -0500

    Remove duplicate folder

--- 241-file-downloads-part-3/FileDownloads-complete/Pods/Target Support Files/RateLimit/RateLimit-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "RateLimit.h"
-
-FOUNDATION_EXPORT double RateLimitVersionNumber;
-FOUNDATION_EXPORT const unsigned char RateLimitVersionString[];
-
