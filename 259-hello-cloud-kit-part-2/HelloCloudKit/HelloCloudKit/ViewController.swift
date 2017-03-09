e7545462605d37939e5299805b32b0a63cc6951f
Ben Scheirman <ben@scheirman.com>
2017-03-09 10:37:02 -0600

    Episode 259

---
+++ 259-hello-cloud-kit-part-2/HelloCloudKit/HelloCloudKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloCloudKit
+//
+//  Created by Ben Scheirman on 1/30/17.
+//  Copyright © 2017 NSScreencast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
